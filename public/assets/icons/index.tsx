df9d97cc215df6d864e15d72273b9b0a77354361
fabricio-dev-front <fabriciopereiradasilva477@gmail.com>
2026-07-19 15:13:14 -0300

    feat: icons

---
+++ public/assets/icons/index.tsx
@@ -0,0 +1,6 @@
+import { IconComments } from "./IconComments";
+import { IconLike } from "./IconLike";
+import { IconFavorites } from "./IconFavorites";
+import { IconRepository } from "./IconRepository";
+
+export { IconComments, IconLike, IconFavorites, IconRepository };
