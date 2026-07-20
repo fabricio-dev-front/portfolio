53ce8a0f8bc4c7de41c81d2568a6306e7c5fa2e2
fabricio-dev-front <fabriciopereiradasilva477@gmail.com>
2026-07-20 15:06:39 -0300

    feat: adding icon

---
+++ public/assets/icons/IconClose.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from "@/types/iconProps";
+
+export function IconClose({ width, height }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width || "24"}
+      height={height || "24"}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
+  );
+}
--- public/assets/icons/index.tsx
+++ public/assets/icons/index.tsx
@@ -2,5 +2,6 @@ import { IconComments } from "./IconComments";
 import { IconLike } from "./IconLike";
 import { IconFavorites } from "./IconFavorites";
 import { IconRepository } from "./IconRepository";
+import { IconClose } from "./IconClose";
 
-export { IconComments, IconLike, IconFavorites, IconRepository };
+export { IconComments, IconLike, IconFavorites, IconRepository, IconClose };
